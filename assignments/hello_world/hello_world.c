9b0c3dc79047da36726e6f607560e2c8c1b32bfb
r-dug <r.douglas.42092@gmail.com>
2024-01-22 11:03:12 -0500

    add hello. why not?

---
+++ assignments/hello_world/hello_world.c
@@ -0,0 +1,11 @@
+/* a note. hello world. the compiler disregards notes
+*/
+
+// another form of note in c. below is an imported library
+
+#include <stdio.h>
+
+int main(){
+    printf("hello world!\n");
+    return 0;
+}
