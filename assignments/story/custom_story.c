9bc292848ddb939cf55800a8f632a73f29785d30
r-dug <r.douglas.42092@gmail.com>
2024-02-13 18:13:03 -0500

    story updates

--- assignments/story/custom_story.c
+++ assignments/story/custom_story.c
@@ -26,18 +26,40 @@ NOTES: This program should tell you a story. I hope it's okay I took the liberty
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 
 struct Reader{
     char name[21];
     int age;
-    char fav_color[11];
+    void *color;
     char *origin;
     char *sign;
     char sidekick[8];
     int keys;
     int tokens;
 };
+char red[] ="\033[1;31m";
 
+char blue[] ="\033[1;34m";
+
+char green[] ="\033[1;32m";
+
+char yellow[] ="\033[1;33m";
+
+char white[] ="\033[1;37m";
+
+char cyan[] ="\033[1;36m";
+
+char purple[] ="\033[1;35m";
+
+void *colors[] = {&red, &blue, &green, &yellow, &white, &cyan, &purple};
+
+void color_reset(){
+    printf("\033[0m");
+}
+void switch_color(char *color){
+    printf("%s", color);
+}
 char earth_poem[] =     "A seed germinates\n"
                         "Nursed by the ground beneath\n"
                         "A hero is born"
@@ -102,117 +124,118 @@ char water_sign[] =
       "                  \n"
 ;
 char fire_sign[] = 
-"                             .:'    .'^,I+],                                                            .:'    .' ,I+],                               \n"
-"                             )c}_~_{ /uYQC)^                            .                               )c}_~_{  uY0C)^                            .  \n"
-"                        `.  `cwUzzULOmpdL  ^      .   `       ,`        I:                        ` .  `cwUzzUC0mpdL ^      .    `      ,`         I: \n"
-"    .^      .^        ':+]I ,ndm0OZwpkbLj?i,`.'``:`  `[I'''   ~         ^(I    .^      .^        ':_]I ,ndm0OZwpkbLj?!,`.'``:`  `[I''' <[~         ^)I\n"
-"I'':<:       il.    .`,l[nn~:fpbqqpdkkOv)_ilIl>~_^  ^t}!Il</r+.          {jI'':<:       il.    .`,l[nn~:fpbqqpdkkZv)_ilIl>~_^  ^t}!Il</r+.          {r\n"
-"(l:~/I       ^|?l   `^ <(u  +)mkbdbkhpJj{-++-{()^  ,uj]__[nz>'.         .<Y|I:~/I       ^|-l ``^ I<(u|c+(mkbdbkhqJj{-++-{()^  ,uj]__[nz>'.         .<X\n"
-"c]<}vi        ?v1_!IIl!]tX>ux}CdkkkkbOYr(}[{/nx:  ,cX  {(xL{''.          ;nX]<}vi        ?v1_!IIl!]tX>ux}LdbkkkbOYr(}[{/nx:  ,cX  {(xL{''.          ;x\n"
-"Lf{fU~        Icv|[~<~~]rC[<CtnmpdbbwLXr/|  zJ-  `tQnjt/xY0}`.           `1Qf{fU~        Icv|[~<~~]rC]>CtnmpdbbwLXr/|  zJ-  `tQnjt/xY0}`.           ^{\n"
-"JJjcL_       .:{Uzf1]???tJz:}LjUmmwqZJcj//rzQz:'`>ULcvnxvUOU~^'.      . .`>UJjcL_       ':{Uzf1]???tJz:}LjUmmwqZJcj//rzQz:''>ULcunxvUOU~^'.      . .`>\n"
-"(cJJ0]^.     `;<|JUn   }1v0/  fCuLOQQ0JvjjrvJZt,^:-JQYzXccXJZL(i,^`'.  ...`;)zJJ0]^'     `;<|JUn   }1v0/ fCuLOQQ0JvjjrvJZt,^:-JQYzXccXJOL|i,^`'.  ..`;\n"
-">~f0Z(;^'.  . l_{cQUv/(()tJQ-IYUXQCUJJcnxvYLZ(:,i_zZJYYUYXYU0ZU/-!:^`..`'',>~f0Z(;^'.  .  _{cQUv/(()tJQ-IYUXQCUJJcnxvYLZ(:,i_zZJYYUYXYU0ZU/-!:^`..`'',\n"
-"::lnwx<;^`..`;_fULLJXx/    Ox;)LYCJXzUzccXJOO)l;<+fZQJYYUUYYYJ0ZQc  I,^ ^' ::lnwx<;^` .`;_fULLJXx/    Ox;)LYCJXzUzccXJOO)l;<_fZQJYYUUUYYJ0ZQc   ,^ ^'^\n"
-"` ,+QJ}i:,''^!fQLJYXvxtt/tu0Xi_CUCUcuYXXYULmJ[ii~_1LZQCUYYUUUYYJQ00J  I;:`^`  ,+QJ[i:,''^!fQLJYXvxft/tu0Xi_CUJUcuYXXUULmJ[ii~_1LZQCUYUUUUYYUQ00J I;:`^\n"
-"'`^;t0r-l;   <YLXcuxrffffjXOv!+JUUXunUYUJCOmx-<~~_{umOQLJYYYUUUYUCJQQt+>I^ ``^;t0r-l;   <YLXvuxrffffjXOv!+JUUXunUUUJCOmx->~~_{umOQLJUYYUUUYUCJQ0t+>I  \n"
-"''` +JL  lI  !vLcnrjjjfjjvQQ);[LzXvxvJJCLQm0|+<+~_{rOZ0QLCUUYUUYUCJJOz{?!,,`'`  JL  lI   vLcnrjjjjfjvQQ)I[LzXcxvJJCLQm0|+<+~_{rOZ0QLCUUYUUYUCUJOz{?!,,\n"
-"''`^;/ZJ|+il::}LUnxxrjjxc0Ot~;/CvvuxcJCLQOwC(+<_~_{nZZ0QLJJUYUUUJUC0Qu|-I:,'.`^;/ZJ|+il::}LUnxxrjjxc0Of~;/CvvuxcJCLQ0wC(+<_~_{nZZ0QLJJJYUUUJUC0Qn   :,\n"
-"''`^,_JqJ)_<iII)LJvnxxvUZQ/-lizYnunxcCCLQOmOt?~__-1XmOQJzXJUYUJUULOLn(_l;:^.'`^,_JqC)_<iII)LJvnxxvUZQ/-lizYnunxcCCLQOmOt?~__-1XmOQJzXJUYUJUULOLn(_!;:^\n"
-"`^^^,~xmwJ|?~<!!1CQYcXLmU)_>;{LunnxxuCCLQQOwC|-?]]   QUvnXJUJUzYQOX|_!I:,`'`^^^,~xmwJ|?~<!!1CQYcXLmU)_>;{LuxnxxuCCLQQOwC|-?]]  mQYvnXJUJUzYQOX|_!I:,`'\n"
-":;;I>_fZOOJ  ~>ii}UZOOZn]~<IicYrxxrrjzCCLLQOmLt}1{rm0XuxnUJJzvXLOv]!I: ``^,:;;Ii_fZOOJ   >ii}YZOOZn]~<IicYrxxrrjzCCLLQOmLt}1{xm0XuxnUJJzvXLOv]iI:  `^,\n"
-"l!i<+1JOQJQU)_ii!>?jJU/_<<iI{LnfjjjjfjvUUJCCQ00ut  ZYnrjnXXuncUZY]!I ``,:;Il!i<+)JOQJQU)_ii!>?jJU/_<<iI{LnfjjjjfjcUUJCCQ00ut  ZYnrjnXXuncUZY]!I,``,:;I\n"
-">~?|uL0LUcYQf_!I;I!<[{_~><iljCrttfffft/xzXXXUUJOUvOQujfjxnrjxvCm/>l,' ;;Ili>~?|uQ0LUcYQt_!I;I!<[{_~><i!jCrttfffft/xzXXXUUJOUvOQujfjxnrjxvCm/>l,' ;;Il!\n"
-" nCQLJYcxxYL(<I;^,;l<+<~i>>ixJr/  tttt  tvcvccuCZ0ZXrftfjjffjnLZ(iI``::;!>+{nCQLJYcxxYL(<I;^,;l~+<~i>>ixJr/  tttt  fvcvccuCZ0ZXrftfjjffjnLO(iI^`::;l>+\n"
-"UOUccujtfvLr+l,   ,:i>>~i<<<fJxt        /vvvufcOOQuf//ttt//tnU0u<I^`::;l<}YOUccujtfuLr+l:    :i>>~i<<<fJxt          /vvvufcOZQuf//ttt//tnU0u<l^`::;l<}\n"
-"QYnnj  tvUt~l,^..  :li<~i<<<)Ynf  (||     rXzv/xCQJrt  /    /rvcLfi;`  ,,Ii{QYnnr   vUt~l,^..^:l!>~i<<<)Ynf  (||  |||rXzv/xCQJrt       /rvcLfi;` ,,Ii}\n"
-"Ucjj|(/vu]!;^`. `^^:l!<~i<~<?nuf ))(|    /rCQJ/fzJXf/|||((|||fxruUf>,`, :l-Ycjj|(/vu]!; `. `^^:l!<<i~~<?nut   )(|    rCQJ/fzJXf/|||((|||fxruUf>,`, :l-\n"
-"fctt1)rr>;,`'  .`` Ili<<i<_<_  /(1{1(|  frYmqZj/nXu/|))){1))(/jttjcr~,^   !fctt))rj>;:`'  .`` Ili<<i<_<_   (1{1(|   rYmqZj/xXu/|))){1)((/jttjcr~,^  :i\n"
-"~xt  [j?::'.   ''^:l!><<><_++)x  ][}(  jnYOwwwx|fvj(){{}[}{1)  |((  x?:^  ,~xt  j?::'.   ''`:l!>~<><_++)x  ][}(   nYOwwwx|fvj(){{}[}{1)   |((  x?:^^^:\n"
-",-j11-(+ ^    ..`,li>~~~~~_-+(f1?_-[)/fnYQQQOOr1/n  }]]--]]{((()1{{1  };^^^,-j11-  ^    ..`,lii~~~~~_-+(f1?_-[)  nYQQQOOr1/n   ]]--]]{)(()1{{1   };^^^\n"
-"^,[([_-_''     '`,i~_?--++_?]  ?~>~?1/rzJJUJLL/](f{]-_+~~_?}}[[[[]?][1/]: `^,[([_-_`'    .'^,i~_?--++_?]  ?~>~]1/rzJJUJLL/](f{]-_+~~_?}}[[[[]?][1/]:^`\n"
-".`l[-+i+'      .^![1|  (1{{)){?<lli-1/nXcvvzUX{~}(?_~>i!i>______+~~+-?[1l^'.`l[-+i+'      .`i[1(  (1{{)){-<lli-1/nXcvvzUX{~}(?_~>i!i>______+~~+-?[1l^'\n"
-" .,~>i;!'      .i?}]]][[}{1[_~!;,,l+}  xjffxvn?!+[~>!lI::Iiii!!!!!i><~-_ .  .,~>i;!'      .i?}]]][[}{1[_<!;,,l+}  xjffxvn?!+[~>!lI::Iiii!!!!!i><~-_ . \n"
-"  ^l ,`,.      ,lIlIIl!ii<il;,^'.`:>-)|(111(/ +,;>i;:,^`'` :, ,,:;Ill!I'     ^l ,`,.      ,lIlIIl!ii<iI;,^'.`:>-(|(111(//+,;>i;:,^`'`    ,,:;Ill!I'   \n"
-"  ''   .       ' ...'```^`'..     `;<-_~~~+_?[i. `^`..     .  .''`^`'        ''   .       ' ...'``^^`...     `;<-_~<<~_?[i. `^`..     .  .''`^`'      \n"
-"                                   ' ^^```^  :`                                                               ' ^^```^  :`                            \n"
+"                                                        .:'    .' ,I+],                         \n"
+"                        .                               )c}_~_{  uY0C)^                         \n"
+"  .   `       ,`        I:                        ` .  `cwUzzUC0mpdL ^      .    `      ,`      \n"
+"`:`  `[I'''   ~         ^(I    .^      .^        ':_]I ,ndm0OZwpkbLj?!,`.'``:`  `[I''' <[~      \n"
+"_^  ^t}!Il</r+.          {jI'':<:       il.    .`,l[nn~:fpbqqpdkkZv)_ilIl>~_^  ^t}!Il</r+.      \n"
+"^  ,uj]__[nz>'.         .<Y|I:~/I       ^|-l ``^ I<(u|c+(mkbdbkhqJj{-++-{()^  ,uj]__[nz>'.      \n"
+"  ,cX  {(xL{''.          ;nX]<}vi        ?v1_!IIl!]tX>ux}LdbkkkbOYr(}[{/nx:  ,cX  {(xL{''.      \n"
+" `tQnjt/xY0}`.           `1Qf{fU~        Icv|[~<~~]rC]>CtnmpdbbwLXr/|  zJ-  `tQnjt/xY0}`.       \n"
+"`>ULcvnxvUOU~^'.      . .`>UJjcL_       ':{Uzf1]???tJz:}LjUmmwqZJcj//rzQz:''>ULcunxvUOU~^'.     \n"
+"^:-JQYzXccXJZL(i,^`'.  ...`;)zJJ0]^'     `;<|JUn   }1v0/ fCuLOQQ0JvjjrvJZt,^:-JQYzXccXJOL|i,^`'.\n"
+"i_zZJYYUYXYU0ZU/-!:^`..`'',>~f0Z(;^'.  .  _{cQUv/(()tJQ-IYUXQCUJJcnxvYLZ(:,i_zZJYYUYXYU0ZU/-!:^`\n"
+"<+fZQJYYUUYYYJ0ZQc  I,^ ^' ::lnwx<;^` .`;_fULLJXx/    Ox;)LYCJXzUzccXJOO)l;<_fZQJYYUUUYYJ0ZQc   \n"
+"~_1LZQCUYYUUUYYJQ00J  I;:`^`  ,+QJ[i:,''^!fQLJYXvxft/tu0Xi_CUJUcuYXXUULmJ[ii~_1LZQCUYUUUUYYUQ00J\n"
+"~_{umOQLJYYYUUUYUCJQQt+>I^ ``^;t0r-l;   <YLXvuxrffffjXOv!+JUUXunUUUJCOmx->~~_{umOQLJUYYUUUYUCJQ0\n"
+"~_{rOZ0QLCUUYUUYUCJJOz{?!,,`'`  JL  lI   vLcnrjjjjfjvQQ)I[LzXcxvJJCLQm0|+<+~_{rOZ0QLCUUYUUYUCUJO\n"
+"~_{nZZ0QLJJUYUUUJUC0Qu|-I:,'.`^;/ZJ|+il::}LUnxxrjjxc0Of~;/CvvuxcJCLQ0wC(+<_~_{nZZ0QLJJJYUUUJUC0Q\n"
+"_-1XmOQJzXJUYUJUULOLn(_l;:^.'`^,_JqC)_<iII)LJvnxxvUZQ/-lizYnunxcCCLQOmOt?~__-1XmOQJzXJUYUJUULOLn\n"
+"]]   QUvnXJUJUzYQOX|_!I:,`'`^^^,~xmwJ|?~<!!1CQYcXLmU)_>;{LuxnxxuCCLQQOwC|-?]]  mQYvnXJUJUzYQOX|_\n"
+"1{rm0XuxnUJJzvXLOv]!I: ``^,:;;Ii_fZOOJ   >ii}YZOOZn]~<IicYrxxrrjzCCLLQOmLt}1{xm0XuxnUJJzvXLOv]iI\n"
+"t  ZYnrjnXXuncUZY]!I ``,:;Il!i<+)JOQJQU)_ii!>?jJU/_<<iI{LnfjjjjfjcUUJCCQ00ut  ZYnrjnXXuncUZY]!I,\n"
+"UvOQujfjxnrjxvCm/>l,' ;;Ili>~?|uQ0LUcYQt_!I;I!<[{_~><i!jCrttfffft/xzXXXUUJOUvOQujfjxnrjxvCm/>l,'\n"
+"Z0ZXrftfjjffjnLZ(iI``::;!>+{nCQLJYcxxYL(<I;^,;l~+<~i>>ixJr/  tttt  fvcvccuCZ0ZXrftfjjffjnLO(iI^`\n"
+"OQuf//ttt//tnU0u<I^`::;l<}YOUccujtfuLr+l:    :i>>~i<<<fJxt          /vvvufcOZQuf//ttt//tnU0u<l^`\n"
+"CQJrt  /    /rvcLfi;`  ,,Ii{QYnnr   vUt~l,^..^:l!>~i<<<)Ynf  (||  |||rXzv/xCQJrt       /rvcLfi;`\n"
+"zJXf/|||((|||fxruUf>,`, :l-Ycjj|(/vu]!; `. `^^:l!<<i~~<?nut   )(|    rCQJ/fzJXf/|||((|||fxruUf>,\n"
+"nXu/|))){1))(/jttjcr~,^   !fctt))rj>;:`'  .`` Ili<<i<_<_   (1{1(|   rYmqZj/xXu/|))){1)((/jttjcr~\n"
+"fvj(){{}[}{1)  |((  x?:^  ,~xt  j?::'.   ''`:l!>~<><_++)x  ][}(   nYOwwwx|fvj(){{}[}{1)   |((  x\n"
+"/n  }]]--]]{((()1{{1  };^^^,-j11-  ^    ..`,lii~~~~~_-+(f1?_-[)  nYQQQOOr1/n   ]]--]]{)(()1{{1  \n"
+"(f{]-_+~~_?}}[[[[]?][1/]: `^,[([_-_`'    .'^,i~_?--++_?]  ?~>~]1/rzJJUJLL/](f{]-_+~~_?}}[[[[]?][\n"
+"}(?_~>i!i>______+~~+-?[1l^'.`l[-+i+'      .`i[1(  (1{{)){-<lli-1/nXcvvzUX{~}(?_~>i!i>______+~~+-\n"
 ;
-int name_num_assign(char *name){
-    int name_num;
-    scanf(name, "%d", name_num);
-    return name_num;
-}
+
 char *origins[] = {"earth", "wind", "water", "fire"};
 
 void printEllipsis(int numPeriods) {
     int i;
     for (i = 0; i < numPeriods; i++) {
         printf(".");
-        fflush(stdout); // Flush the output buffer to ensure the dot is printed immediately
-        sleep(1);       // Sleep for one second
+        fflush(stdout); 
+        sleep(1);       
     }
     printf("\n");
 }
 void typeMessage(char *str, int speed) {
     while (*str != '\0') {
-        putchar(*str);   // Print the current character
-        fflush(stdout);  // Flush the output buffer to ensure immediate printing
-        usleep((80000*(rand()%5))/speed);  // Sleep for 100 milliseconds (100000 microseconds)
-        str++;           // Move to the next character in the string
+        putchar(*str);   
+        fflush(stdout);  
+        usleep((80000*(rand()%5))/speed);  
+        str++;           
     }
 }
 int main()
 {   
+    srand(time(NULL));
     system("clear");
-    struct Reader character;
-    // messages
+    struct Reader you;
+    you.tokens = 20;
+    you.keys = 0;
     char current_message[1000];
-    sprintf(current_message, "A mysterious force has poisoned our world... Little time remains");
-    typeMessage(current_message, 1);
+
+    sprintf(current_message, "\033[1;35m Your conciousness awakes in a void. \n You hear a voice, solemn and desparate...\033[0m \n\n\tA mysterious force has poisoned our world... Little time remains");
+    typeMessage(current_message, 2);
     printEllipsis(3);
-    sprintf(current_message, "What is your name, brave soul?\n");
-    typeMessage(current_message, 1);
-    scanf("%s", character.name);
+    sprintf(current_message, "\tWhat is your name, brave soul?\n");
+    typeMessage(current_message, 2);
+    scanf("%s", you.name);
     sleep(1);
-    int name_num = name_num_assign(character.name);
-    character.origin = origins[ name_num ];
-    sprintf(current_message, "Welcome, %s - hero of %s", character.name, character.origin);
-    typeMessage(current_message, 1);
+    int name_num = rand()%4;
+    you.color = colors[ name_num ];
+    you.origin = origins[ name_num ];
+    sprintf(current_message, "Welcome, %s - hero of %s", you.name, you.origin);
+    typeMessage(current_message, 2);
     printEllipsis(3);
     switch(name_num){
         case 0:
-            character.sign = earth_sign;
+            you.sign = earth_sign;
             break;
         case 1:
-            character.sign = wind_sign;
+            you.sign = wind_sign;
             break;
         case 2:
-            character.sign = water_sign;
+            you.sign = water_sign;
             break;
         case 3:
-            character.sign = fire_sign;
+            you.sign = fire_sign;
             break;
     }
     
     printEllipsis(5);
     // show an animation based on origin
     system("clear");
-    typeMessage(character.sign, 10);
+    switch_color(you.color);
+    typeMessage(you.sign, 20);
     switch(name_num){
         case 0:
+            switch_color(you.color);
             typeMessage(earth_poem, 1);
             break;
         case 1:
+            switch_color(you.color);
             typeMessage(wind_poem, 1);
             break;
         case 2:
+            switch_color(you.color);
             typeMessage(water_poem, 1);
             break;
         case 3:
+            switch_color(you.color);
             typeMessage(fire_poem, 1);
             break;
     }
-    
+    color_reset();
     system("clear");
 
     sprintf(current_message, "Wowee Zowee! You look strong....\nLet's see if your brains are as able as your muscles.\n");
