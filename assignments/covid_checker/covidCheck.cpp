7455da1bda964ed241e5394c9e6ef534bdff3133
r-dug <r.douglas.42092@gmail.com>
2024-04-15 16:41:51 -0400

    update

---
+++ assignments/covid_checker/covidCheck.cpp
@@ -0,0 +1,72 @@
+/*
+Class: CSE 130 | programming in C and C++
+Language: C++
+
+Program discription: 
+This program is a covid symptom checker. 
+Print the following table that shows the symptoms for COVID-19, Cold, and Flu.
+• Ask the user to enter the number of patients he/she wants to check.
+• Generate Symptoms Randomly for each patient.
+• Seven Symptoms: Each Symptom has value ["present", "absent"].
+• After generating Symptoms randomly for all patients please check each patient if he/she
+has COVID-19, Cold, or Flu.
+• If the patient’s symptoms do not match with symptoms of COVID-19, Cold, or Flu then
+he/she has other illness.
+• A final report stating:
+1. How many patients have COVID-19
+2. How many patients have Cold
+3. How many patients have Flu
+4. How many patients have other illness
+5. The percentage of each illness 
+
+
+Guide: * Common + Sometimes/Rarely - NO
+==============================================================
+Symptoms | COVID-19 | Cold | Flu |
+==============================================================
+Fever | * | + | * |
+Cough | * | + | * |
+Shortness of Breath | * | - | - |
+Runny Nose | + | * | + |
+Headaches | + | + | * |
+Sneezing | - | * | - |
+Fatigue | + | + | * |
+==============================================================
+
+Enter the number of patients? 100
+Thank you...
+========================================
+Symptoms Checker....
+2 patients have symptoms of COVID-19
+8 patients have symptoms of Cold
+8 patients have symptoms of Flu
+82 patients may have some other illness
+========================================
+Percentage of each illness:
+COVID-19: [2%]++
+Cold: [8%]++++++++
+Flu: [8%]++++++++
+Other illness:[82%]+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++++++++++++++++++++
+
+
+Author: Richard Douglas
+Start date: 04/15/2024
+Last edit: 04/15/2024
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <limits>
+
+using namespace std;
+
+// class for symptom checker
+
+
+// class for patient
+
+
+// Main function block
+
